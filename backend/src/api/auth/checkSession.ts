afb21dd7089fbc99f0fa89f6f955f5cf864ab29d
realeldermann <julian@jochens.eu>
2022-08-29 00:40:19 +0200

    Changed checkSession.ts

--- backend/src/api/auth/checkSession.ts
+++ backend/src/api/auth/checkSession.ts
@@ -4,18 +4,12 @@ import User from "../../db/schemas/User.schema";
 import { Types } from "mongoose";
 
 export async function checkSessionUser(args: {sessionToken: string}){
-    const sessionUser = await Session.find({ token: args.sessionToken }, 'user -_id');
-    let sessionUserString = JSON.stringify(sessionUser);
-    sessionUserString = sessionUserString.replace('[{"user":"', '')
-    sessionUserString = sessionUserString.replace('"}]', '')
-    return sessionUserString
+    const sessionUser = await Session.findOne({ token: args.sessionToken }, 'user -_id');
+    return sessionUser?.user
 }
 
 export async function checkSessionUserCourses(args: {sessionToken: string}){
     const sessionUser = await checkSessionUser({sessionToken: args.sessionToken})
-    const sessionUserCourse = await User.find({  _id: new Types.ObjectId(sessionUser)  }, 'course -_id');
-    let sessionUserCourseString = JSON.stringify(sessionUserCourse);
-    sessionUserCourseString = sessionUserCourseString.replace('[{"course":"', '')
-    sessionUserCourseString = sessionUserCourseString.replace('"}]', '')
-    return sessionUserCourseString
+    const sessionUserCourse = await User.findOne({  _id: new Types.ObjectId(sessionUser?._id)  }, 'course -_id');
+    return sessionUserCourse?.course
 }
